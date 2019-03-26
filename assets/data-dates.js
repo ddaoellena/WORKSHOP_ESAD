465e38c51eee647052ce1ac74db27bab36ecb154
Rémy Lemaire <homieremy@gmail.com>
2019-03-26 11:41:55 +0100

    Update data-dates.js

--- assets/data-dates.js
+++ assets/data-dates.js
@@ -1,62 +1,110 @@
 var date1 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Invention de la publicité",
+  date : "500 avant J.C",
+  content : "Des jarres vantant les mérites d’un homme politique en Grèce antique sont créées.",
   position : ""
 }
 
 var date2 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Les crieurs publics",
+  date : "550",
+  content : " les décisions royales sont prononcées par un crieur public, formant la publicité de l’époque.",
   position : ""
 }
 
 var date3 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Invention de l'imprimerie",
+  date : "1450",
+  content : "Avancée majeure dans la pub, on peut maintenant afficher des messages.",
   position : ""
 }
 
 var date4 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "La publicité dans la presse",
+  date : "1660",
+  content : "Le Mercurius Politicus publie la première publicité dans un périodique.",
   position : ""
 }
 
 var date5 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Démocratisation de l'affiche",
+  date : "XIXè siècle",
+  content : "Toulouse Lautrec et Leonetto Cappiello y contribuent grandement en simplifiant, chacun à leur manière, les formes et les couleurs afin de rendre les affiches plus attirantes.",
   position : ""
 }
 
 var date6 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Première publicité filmée",
+  date : "1898",
+  content : "Félix Mesguisch installe une caméra projecteur, la première pub vidéo de l’Histoire.",
   position : ""
 }
 
 var date7 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Première publicité radio",
+  date : "1923",
+  content : "Citroën lance la première publicité radiophonique",
   position : ""
 }
 
 var date8 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Avènement de la consommation de masse",
+  date : "1936",
+  content : "La revalorisation des salaires, la semaine de 40 heures et les congés payés.",
   position : ""
 }
 
 var date9 = {
-  titre : "",
-  date : "",
+  titre : "Publicité à la télévision",
+  date : "1968",
   content : "",
+  position : ""
+}
+var date10 = {
+  titre : "Prémices de la réalité augmentée",
+  date : "1980",
+  content : "Steve Mann",
+  position : ""
+}
+  var date11 = {
+    titre : "Invention du World Wide Web",
+    date : "1989",
+    content : "Tim Berners-Lee",
+    position : ""
+}
+var date12 = {
+  titre : "Mise en place de l'appellation «Big Data»",
+  date : "1997",
+  content : "",
+  position : ""
+}
+var date13 = {
+  titre : "Lancement de Google",
+  date : "1997",
+  content : "Larry Page et Sergey Brin",
+  position : ""
+}
+var date14 = {
+  titre : "Début de la publicité en ligne (en France)",
+  date : "1998",
+  content : "",
+  position : ""
+}
+var date15 = {
+  titre : "Invention de Facebook",
+  date : "2004",
+  content : "Mark Zuckerberg",
+  position : ""
+}
+var date16 = {
+  titre : "Création de Twitter",
+  date : "2006",
+  content : "Jack Dorsey, Evan Williams, Biz Stone et Noah Glass",
+  position : ""
+}
+var date17 = {
+  titre : "Création d'Instagram",
+  date : "2010",
+  content : "Kevin Systrom",
   position : ""
 }
