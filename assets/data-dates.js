77a1c08d97cea40af3a328ac818d03942d2ff130
Rémy Lemaire <homieremy@gmail.com>
2019-03-26 15:11:21 +0100

    Update data-dates.js

--- assets/data-dates.js
+++ assets/data-dates.js
@@ -8,14 +8,14 @@ var date1 = {
 var date2 = {
   titre : "Les crieurs publics",
   date : "550",
-  content : " les décisions royales sont prononcées par un crieur public, formant la publicité de l’époque.",
+  content : "les décisions royales sont prononcées par un crieur public, formant la publicité de l’époque.",
   position : ""
 }
 
 var date3 = {
   titre : "Invention de l'imprimerie",
   date : "1450",
-  content : "Avancée majeure dans la pub, on peut maintenant afficher des messages.",
+  content : "Par Gutenberg à Mayence, avancée majeure dans la pub, on peut maintenant afficher des messages.",
   position : ""
 }
 
@@ -50,61 +50,61 @@ var date7 = {
 var date8 = {
   titre : "Avènement de la consommation de masse",
   date : "1936",
-  content : "La revalorisation des salaires, la semaine de 40 heures et les congés payés.",
+  content : "Le Front Populaire revalorise les salaires, et met en place la semaine de 40 heures et les congés payés.",
   position : ""
 }
 
 var date9 = {
   titre : "Publicité à la télévision",
   date : "1968",
-  content : "",
+  content : "Le premier spot publicitaire diffusé à la télévision est pour la marque de fromage Boursin.",
   position : ""
 }
 var date10 = {
   titre : "Prémices de la réalité augmentée",
   date : "1980",
-  content : "Steve Mann",
+  content : "Steve Mann invente le EyeTap, un device permettant d’afficher des informations virtuelles. Il s’agit d’une sorte de lunette, encadrant un seul oeil.",
   position : ""
 }
   var date11 = {
     titre : "Invention du World Wide Web",
     date : "1989",
-    content : "Tim Berners-Lee",
+    content : "Par Tim Berners-Lee. À l'origine, le projet, baptisé « World Wide Web », a été conçu et développé pour que des scientifiques travaillant dans des universités et instituts du monde entier puissent s'échanger des informations instantanément.",
     position : ""
 }
 var date12 = {
   titre : "Mise en place de l'appellation «Big Data»",
   date : "1997",
-  content : "",
+  content : "La notion de Big Data est utilisée pour traduire le fait que les entreprises sont confrontées à des volumes de données (data) à traiter de plus en plus considérables et présentant de forts enjeux commerciaux et marketing.",
   position : ""
 }
 var date13 = {
   titre : "Lancement de Google",
   date : "1997",
-  content : "Larry Page et Sergey Brin",
+  content : "Par Larry Page et Sergey Brin, dans un petit garage de Californie.",
   position : ""
 }
 var date14 = {
   titre : "Début de la publicité en ligne (en France)",
   date : "1998",
-  content : "",
+  content : "Elle apparait en 1998, mais explose réellement dans les années 2000 avec la démocratisation du web",
   position : ""
 }
 var date15 = {
   titre : "Invention de Facebook",
   date : "2004",
-  content : "Mark Zuckerberg",
+  content : "Mark Zuckerberg créé un site pour les étudiants d'Harvard pour commencer, puis pour les étudiants d'autres universités américaines",
   position : ""
 }
 var date16 = {
   titre : "Création de Twitter",
   date : "2006",
-  content : "Jack Dorsey, Evan Williams, Biz Stone et Noah Glass",
+  content : "Par Jack Dorsey, Evan Williams, Biz Stone et Noah Glass. Le service est rapidement devenu populaire, jusqu'à réunir plus de 500 millions d'utilisateurs dans le monde fin février 2012.",
   position : ""
 }
 var date17 = {
   titre : "Création d'Instagram",
   date : "2010",
-  content : "Kevin Systrom",
+  content : "Kevin Systrom, fan de photographie, décide de créer une application de partage de photos, alors qu'il était étudiant à l'université Stanford.",
   position : ""
 }
