3b0611121f591aa5e340dd1f75c12928f54e9235
wtrknight <34352734+wtrknight@users.noreply.github.com>
2019-03-27 14:49:10 +0100

    Update data-content-up.js

--- assets/data-content-up.js
+++ assets/data-content-up.js
@@ -1,71 +1,142 @@
 var contentUp0 = {
-  titre : "Panneau publicitaire ",
-  date : "1990",
-  content : "Le panneau publicitaire a été mis en place dans les années 1990 à Time Square, New York. C’est le quartier où la concentration d’enseignes lumineuses est la plus haute.",
+  titre : "Invention de la publicité",
+  date : "500 av. JC",
+  content : "Les jarres vantant les mérites d’un homme politique en Grèce antique.",
   pic : "",
   position : "",
   dataviz : false,
 }
 
 var contentUp1 = {
-  titre : "Hologramme publicitaire ",
-  date : "2010",
-  content : "L’hologramme publicitaire a été mis en place en 2010, par la société Kino-Mo, sous la forme d’un ventilateur qui affiche un hologramme.",
+  titre : "Les crieurs publics",
+  date : "550",
+  content : "Les décisions royales sont prononcées par un crieur public, formant la publicité de l’époque.",
   pic : "",
   position : "",
   dataviz : false,
 }
 
 var contentUp2 = {
-  titre : "Panneau d’affichage volant",
-  date : "2014",
-  content : "Le panneau d’affichage volant a été mis en place par la société DroneCast en 2014",
+  titre : "Invention de l’imprimerie",
+  date : "1450",
+  content : "Avancée majeure dans la pub, on peut maintenant afficher des messages.",
   pic : "",
   position : "",
   dataviz : false,
 }
-
 var contentUp3 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "La pub dans la presse",
+  date : "1660",
+  content : "Le Mercurius Politicus publie la première publicité dans un périodique.",
   pic : "",
   position : "",
   dataviz : false,
 }
-
 var contentUp4 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Démocratisation de l’affiche",
+  date : "XIXe siècle",
+  content : "Toulouse Lautrec et Leonetto Cappiello y contribuent grandement en simplifiant, chacun à leur manière, les formes et les couleurs afin de rendre les affiches plus attirantes.",
   pic : "",
   position : "",
   dataviz : false,
 }
-
 var contentUp5 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Première publicité filmée",
+  date : "1898",
+  content : "Félix Mesguisch installe une caméra projecteur, la première pub vidéo de l’Histoire.",
   pic : "",
   position : "",
   dataviz : false,
 }
-
 var contentUp6 = {
-  titre : "",
-  date : "",
-  content : "",
+  titre : "Première pub radio",
+  date : "1923",
+  content : "Citroën lance la première publicité radiophonique.",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp7 = {
+  titre : "Avènement de la consommation de masse",
+  date : "1936",
+  content : "La revalorisation des salaires, la semaine de 40 heures et les congés payés.",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp8 = {
+  titre : "Publicité à la télévision",
+  date : "1968",
+  content : "Félix Mesguisch installe une caméra projecteur, la première pub vidéo de l’Histoire.",
   pic : "",
   position : "",
   dataviz : false,
 }
 
-var contentUp7 = {
-  titre : "",
-  date : "",
+var contentUp9 = {
+  titre : "Prémices de la réalité augmentée",
+  date : "1980",
+  content : "Steve Mann",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp10 = {
+  titre : "Invention du World Wide Web",
+  date : "1989",
+  content : "Tim Berners-Lee",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp11 = {
+  titre : "Mise en place de l’appellation “Big Data”",
+  date : "1997",
   content : "",
   pic : "",
   position : "",
   dataviz : false,
 }
+var contentUp12 = {
+  titre : "Lancement de Google",
+  date : "1997",
+  content : "Larry Page et Sergey Brin",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp13 = {
+  titre : "Début de la pub sur Internet (en France)",
+  date : "1998",
+  content : "",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp14 = {
+  titre : "Invention de Facebook",
+  date : "2004",
+  content : "Mark Zuckerberg",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp15 = {
+  titre : "Création de Twitter",
+  date : "2006",
+  content : "Jack Dorsey, Evan Williams, Biz Stone et Noah Glass",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+var contentUp16 = {
+  titre : "Création d’Instagram",
+  date : " 2010",
+  content : "Kevin Systrom",
+  pic : "",
+  position : "",
+  dataviz : false,
+}
+
+
+
